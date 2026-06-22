e4ed570e7cfb035ccc15a73d18391bc8c0db25c9
LUZGIANINA <peqush@gmail.com>
2026-06-21 23:17:20 -0500

    Remove space in import statement for sklearn

--- Modelo_Merrill_Crowe.py
+++ Modelo_Merrill_Crowe.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split,cross_val_score
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
